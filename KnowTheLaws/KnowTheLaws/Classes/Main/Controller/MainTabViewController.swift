1a267913b51bdd1d6060ba94f4389576705f932d
Kingslanding <orioncaptaindev@yahoo.com>
2015-09-22 13:39:05 +0800

    create main UITabBarController

---
+++ KnowTheLaws/KnowTheLaws/Classes/Main/Controller/MainTabViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabViewController.swift
+//  KnowTheLaws
+//
+//  Created by King's Landing on 9/22/15.
+//  Copyright © 2015 King's Landing. All rights reserved.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
